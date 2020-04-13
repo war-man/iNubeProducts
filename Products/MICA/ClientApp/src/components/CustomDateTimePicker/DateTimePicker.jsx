b6b92b7fd575f381f9244f8fb984601b3d5b2842
GopiKrishna S <gopi@inubesolutions.com>
2020-04-13 13:24:15 +0530

    Claim_inbox_changes

--- Products/MICA/ClientApp/src/components/CustomDateTimePicker/DateTimePicker.jsx
+++ Products/MICA/ClientApp/src/components/CustomDateTimePicker/DateTimePicker.jsx
@@ -4,9 +4,9 @@ import { DatePicker, DateTimePicker } from "@material-ui/pickers";
 import MomentUtils from '@date-io/moment';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 function CustomDateTimePicker(props) {
-    const { timeformate, labelText, disabled, name, required, id, onChange, value, width, disableFuture} = props;
-    
-  
+    const { timeformate, labelText, disabled, name, required, id, onChange, value, width, isClearable, minDate, maxDate, disableFuture } = props;
+
+
     return (
         <MuiPickersUtilsProvider utils={MomentUtils} >
             {(timeformate) ?
@@ -17,42 +17,46 @@ function CustomDateTimePicker(props) {
                     name={name}
                     id="DateTimePicker"
                     required={required}
+                    minDate={minDate}
+                    maxDate={maxDate}
                     value={(value == undefined) ? null : value}
                     format="DD/MM/YYYY hh:mm a"
                     autoOk
-                        disabled={disabled}
-                        disableFuture={disableFuture}
-                        style={{ 'top': '11px', 'width': width }}
+                    disabled={disabled}
+                    disableFuture={disableFuture}
+                    style={{ 'top': '11px', 'width': width }}
+                    isClearable={isClearable}
+                    //  editFormat={formatter} 
+                    onChange={onChange}
+                />
+                :
+
+                <DatePicker
+                    variant="inline"
+                    label={labelText}
+                    value={(value == undefined) ? null : value}
+                    name={name}
+                    className="DateTimePicker"
+                    id="DateTimePicker"
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    format="DD/MM/YYYY"
+                    autoOk
+                    disabled={disabled}
+                    disableFuture={disableFuture}
+                    style={{ 'top': '10px', 'width': width }}
+                    isClearable={isClearable}
                     //  editFormat={formatter} 
                     onChange={onChange}
 
 
                 />
-            :
-                
-                    <DatePicker
-                        variant="inline"
-                        label={labelText}
-                        value={(value == undefined) ? null : value}
-                        name={name}
-                        className="DateTimePicker"
-                        id="DateTimePicker"
-                        format="DD/MM/YYYY"
-                        autoOk
-                        disabled={disabled}
-                        disableFuture={disableFuture}
-                        style={{ 'top': '10px', 'width': width }}
-                        //  editFormat={formatter} 
-                        onChange={onChange}
 
-
-                    />
-                
             }
-          
 
-            </MuiPickersUtilsProvider>
-      
+
+        </MuiPickersUtilsProvider>
+
     );
 }
 
