2152363b1ab8333dacabd60d989f369e087dc3b0
Nadira Khanum <nadira.khanum@inubesolutions.com>
2020-04-15 14:15:00 +0530

    File Upload

--- Products/MICA/ClientApp/src/modules/FileUpload/views/DocumentUpload.jsx
+++ Products/MICA/ClientApp/src/modules/FileUpload/views/DocumentUpload.jsx
@@ -2,14 +2,23 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import swal from 'sweetalert';
-import BillingConfig from "modules/Billing/BillingConfig.js";
 import CardHeader from "components/Card/CardHeader.jsx";
 import Card from "components/Card/Card.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
-
+import GridItem from "components/Grid/GridItem.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import ReactTable from "components/MuiTable/MuiTable.jsx";
+import policyConfig from 'modules/Policy/PolicyConfig.js';
+import TranslationContainer from "components/Translation/TranslationContainer.jsx";
+import FileUploadConfig from "modules/FileUpload/FileUploadConfig.js";
 
 const MyUploader = (props) => {
-   
+
+    let [TableDataCopy, RefundTableHeaderFun] = React.useState([]);
+    let [ShowGrid, GridFun] = React.useState(false);
+    let [errorList, errorListFun] = React.useState([]);
+    let TableDataList = [];
+    console.log("doc props ", props)
     // specify upload params and url for your files
     const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
 
@@ -18,64 +27,134 @@ const MyUploader = (props) => {
 
     // receives array of files that are done uploading when submit button is clicked
     const handleSubmit = (files) => {
-        console.log("SubmitData", files.map(f => f.meta))   
+        console.log("SubmitData", files.map(f => f.meta))
         var data = new FormData();
         if (files.length > 0) {
             for (var i = 0; i < files.length; i++) {
                 data.append(files[i].file.name, files[i].file);
+
             }
         }
-        debugger
+
         $.ajax({
             type: "POST",
-            url: `${BillingConfig.BillingConfigUrl}/api/Billing/UploadFiles/UploadFiles?contractId=`+props.contractId,
+            url: `${FileUploadConfig.FileUploadConfigUrl}/api/Mica_EGI/MonthlySIUpload`,
+            //https://localhost:44351/api/Policy/RefundUpload
+            //url: `https://localhost:44351/api/Policy/RefundUpload`,
             contentType: false,
             processData: false,
+
             data: data,
             beforeSend: function (xhr) {
                 /* Authorization header */
                 xhr.setRequestHeader("Authorization", 'Bearer ' + localStorage.getItem('userToken'));
-                
             },
-            success: function () {
-                swal({
-                    text: "Document Uploaded Successful",
-                    icon: "success"
-                }); 
-                //history.push('/');
+            success: function (response) {
+                console.log("response ", response);
+
+                if (response.status == 1) {
+                    GridFun(false);
+                    swal({
+
+                        text: response.responseMessage,
+                        icon: "success"
+                    });
+                } else if (response.status == 7) {
+                    if (response.errorDetails.length > 0) {
+                        GridFun(true);
+                        errorListFun(response.errorDetails);
+                        RefundTableHeader(response.errorDetails);
+                    }
+                    swal({
+                        text: response.responseMessage,
+                        icon: "success"
+                    });
+
+                }
+                else {
+                }
             },
             error: function () {
+                GridFun(false);
                 swal({
-                    text: "Document Uploaded Failed",
+                    text: "File uploading unsuccessful",
                     icon: "error"
-                }); 
-                //alert("There was error uploading files!");
+                });
             }
         });
 
 
     }
 
-    return (
-        <div>
-        <GridContainer>
-            <CardHeader color="info" icon >
+    const RefundTableHeader = (activityDTOs) => {
 
-                {
+        TableDataList = Object.keys(activityDTOs[0]).map((prop, key) => {
+            return {
+                Header: prop.charAt(0).toUpperCase() + prop.slice(1),
+                accessor: prop,
+            };
+
+        })
+        RefundTableHeaderFun(TableDataList);
+
+        console.log("table data", TableDataList, errorList);
+    }
+
+    return (
+        <GridContainer lg={12}>
+            <Card>
+                <CardHeader color="info" icon >
+
+
+
                     <h3 >
-                        <small> Documents </small>
+                        <small> Upload Document:</small>
+
+                        <span id="red" style={{ color: 'red' }}>*</span>
                     </h3>
-                }
-            </CardHeader>
-       
-            <Dropzone
-                    getUploadParams={getUploadParams}
-                    onChangeStatus={handleChangeStatus}
-                    onSubmit={handleSubmit}
-                    //contractId={props.contractId}
-                />
-            </GridContainer>
-        </div>
+
+                </CardHeader>
+
+                <CardBody>
+                    <GridContainer justify="center">
+
+                        <GridItem xs={12}>
+                            <Dropzone
+                                maxFiles={1}
+                                getUploadParams={getUploadParams}
+                                onChangeStatus={handleChangeStatus}
+                                onSubmit={handleSubmit}
+                            //accept="image/*,audio/*,video/*,application/pdf/*,word/*"
+                            />
+                        </GridItem>
+                    </GridContainer>
+
+
+                    {ShowGrid && <GridContainer justify="center" >
+                        <GridItem xs={12}>
+
+                            <ReactTable
+                                title={<h5><TranslationContainer translationKey={"Refund Upload Errors"} /></h5>}
+
+                                data={errorList}
+                                filterable
+                                columns={TableDataCopy}
+                                defaultPageSize={4}
+                                pageSize={([errorList.length + 1] < 4) ? [errorList.length + 1] : 4}
+                                showPaginationTop={false}
+                                //showPaginationBottom={([this.state.data.length + 1] <= 5) ? false : true}
+                                showPaginationBottom={true}
+                                className="-striped -highlight discription-tab"
+
+                            />
+
+                        </GridItem>
+
+                    </GridContainer>}
+                </CardBody>
+            </Card>
+        </GridContainer>
     );
 }
+
 export default MyUploader;
--- Products/MICA/ClientApp/src/modules/FileUpload/views/Upload.jsx
+++ Products/MICA/ClientApp/src/modules/FileUpload/views/Upload.jsx
@@ -31,6 +31,7 @@ import TranslationContainer from "components/Translation/TranslationContainer.js
 import Dropdown from "components/Dropdown/Dropdown.jsx";
 import MyUploader from "modules/FileUpload/views/DocumentUpload.jsx";
 import FileUploadConfig from "modules/FileUpload/FileUploadConfig.js";
+import Dropzone from 'react-dropzone-uploader';
 
 class Upload extends React.Component {
     constructor(props) {
@@ -45,9 +46,28 @@ class Upload extends React.Component {
                 FileName:"",
             },
             showUpload: false,
-            monthly:[],
+            monthly: [],
+            GridFun: false,
+            ShowGrid: false,
+            errorListFun: [],
+            errorList: [],
+            TableDataList: [],
+            TableDataCopy: [],
+            RefundTableHeaderFun:[],
         };
+        //let [TableDataCopy, RefundTableHeaderFun] = React.useState([]);
+        //let [ShowGrid, GridFun] = React.useState(false);
+        //let [errorList, errorListFun] = React.useState([]);
+        //let TableDataList = [];
     }
+
+
+ 
+// specify upload params and url for your files
+ getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+
+// called every time a file's `status` changes
+ handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
 
     onInputParamChange = (evt) => {
         console.log('Event', evt);
@@ -55,78 +75,135 @@ class Upload extends React.Component {
         fields[evt.target.name] = evt.target.value;
         this.setState({ fields });
         this.state.showUpload = true;
-        var data = evt.target.value;
-        if (evt.target.value == "Monthly SI Upload") {
-            fetch(`${FileUploadConfig.FileUploadConfigUrl}/api/Mica_EGI/MonthlySIUpload`, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+    }
+
+// receives array of files that are done uploading when submit button is clicked
+    handleSubmit = (files) => {
+        debugger
+        console.log("SubmitData", files.map(f => f.meta))
+        var data = new FormData();
+        if (files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+                data.append(files[i].file.name, files[i].file);
+
+            }
+        }
+
+     
+        if (this.state.fields.FileName == 1) {
+            $.ajax({
+                type: "POST",
+                url: `${FileUploadConfig.FileUploadConfigUrl}/api/Policy/RefundUpload`,
+                //https://localhost:44351/api/Policy/RefundUpload
+                //url: `https://localhost:44351/api/Policy/RefundUpload`,
+                contentType: false,
+                processData: false,
+
+                data: data,
+                beforeSend: function (xhr) {
+                    /* Authorization header */
+                    xhr.setRequestHeader("Authorization", 'Bearer ' + localStorage.getItem('userToken'));
                 },
-                body: JSON.stringify("Monthly SI")
-            }).then(response => response.json())
-                .then(data => {
+                success: function (response) {
+                    console.log("response ", response);
+
+                    if (response.status == 1) {
+
+                        this.state.GridFun = false;
+                        swal({
+
+                            text: response.responseMessage,
+                            icon: "success"
+                        });
+                    } else if (response.status == 7) {
+                        if (response.errorDetails.length > 0) {
+                            this.state.GridFun = true;
+                            this.state.errorListFun(response.errorDetails);
+                            this.state.RefundTableHeader(response.errorDetails);
+                        }
+                        swal({
+                            text: response.responseMessage,
+                            icon: "success"
+                        });
+                        console.log("check", response.responseMessage);
+                    }
+                    else {
+                    }
+                },
+                error: function () {
+                    this.state.GridFun = false;
                     swal({
-
-                        text: data.response,
-                        icon: "success"
-                    }); 
-                    if (data.status == 200) {
-                        swal({
-                            text: "Report Modified!",
-                            icon: "success"
-                        });
-                    } else if (data.status == 8) {
-                        swal({
-                            text: data.errors[0].errorMessage,
-                            icon: "error"
-                        });
-                    } else if (data.status == 4) {
-                        swal({
-                            text: data.errors[0].errorMessage,
-                            icon: "error"
-                        });
-                    }
-
-                    console.log("Search history data:", data);
-
-                });
+                        text: "File uploading unsuccessful",
+                        icon: "error"
+                    });
+                }
+            });
         }
+
         else {
-            fetch(`${FileUploadConfig.FileUploadConfigUrl}/api/Policy/RefundUpload`, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+            $.ajax({
+                type: "POST",
+                url: `${FileUploadConfig.FileUploadConfigUrl}/api/Mica_EGI/MonthlySIUpload`,
+                //https://localhost:44351/api/Policy/RefundUpload
+                //url: `https://localhost:44351/api/Policy/RefundUpload`,
+                contentType: false,
+                processData: false,
+
+                data: data,
+                beforeSend: function (xhr) {
+                    /* Authorization header */
+                    xhr.setRequestHeader("Authorization", 'Bearer ' + localStorage.getItem('userToken'));
                 },
-                body: JSON.stringify("Refund Report")
-            }).then(response => response.json())
-                .then(data => {
-                    if (data.status == 200) {
+                success: function (response) {
+                    console.log("response ", response);
+
+                    if (response.status == 1) {
+
+                        this.state.GridFun = false;
                         swal({
 
-                            text: "Policy Modified!",
+                            text: response.responseMessage,
                             icon: "success"
                         });
-                    } else if (data.status == 8) {
+                    } else if (response.status == 7) {
+                        if (response.errorDetails.length > 0) {
+                            this.state.GridFun = true;
+                            this.state.errorListFun(response.errorDetails);
+                            this.state.RefundTableHeader(response.errorDetails);
+                        }
                         swal({
-                            text: data.errors[0].errorMessage,
-                            icon: "error"
+                            text: response.responseMessage,
+                            icon: "success"
                         });
-                    } else if (data.status == 4) {
-                        swal({
-                            text: data.errors[0].errorMessage,
-                            icon: "error"
-                        });
+
                     }
-
-                    console.log("Search history data:", data);
-
-                });
+                    else {
+                    }
+                },
+                error: function () {
+                    this.state.GridFun = false;
+                    swal({
+                        text: "File uploading unsuccessful",
+                        icon: "error"
+                    });
+                }
+            });
         }
- }
+}
+
+     RefundTableHeader = (activityDTOs) => {
+
+        this.state.TableDataList = Object.keys(activityDTOs[0]).map((prop, key) => {
+            return {
+                Header: prop.charAt(0).toUpperCase() + prop.slice(1),
+                accessor: prop,
+            };
+
+        })
+        this.state.RefundTableHeaderFun(this.state.TableDataList);
+
+        console.log("table data", this.state.TableDataList, this.state.errorList);
+    }
 
     render() {
         return (
@@ -152,38 +229,75 @@ class Upload extends React.Component {
                                             id="FileName"
                                             value={this.state.fields.FileName}
                                             lstObject={this.state.typeList}
-                                            //required={true}
-                                            //model="InvoiceSearchHistory"
-                                            //filterName='InvoiceStatus'
                                             name='FileName'
-                                            onChange={this.onInputParamChange}
+                                           onChange={this.onInputParamChange}
+                                           // onChange={this.handleSubmit}
                                             formControlProps={{
                                                 fullWidth: true
                                             }}
                                         />
                                     </GridItem>
                             </GridContainer>
-
-                            {this.state.showUpload &&
-                                    <MyUploader
-                                        handleChange={this.handleChange}
-                                        getUploadParams={this.state.getUploadParams}
-                                        onChangeStatus={this.state.handleChangeStatus}
-                                        onSubmit={this.state.handleSubmit}
-                                        fields={this.state.fields}
-                                        //ContractData={this.state.contractId}
-                                        //contractId={this.state.contractId}
-                                        onInputParamChange={this.onInputParamChange}
-                                    />
-                            }
-
                         </div>
                             </CardBody>
                 </Card>
 
-               
-                        </div>
+                {this.state.showUpload &&
+                    <Card>
+                        <CardHeader color="info" icon >
 
+
+
+                            <h3 >
+                                <small> Upload Document:</small>
+
+                                <span id="red" style={{ color: 'red' }}>*</span>
+                            </h3>
+
+                        </CardHeader>
+
+                        <CardBody>
+
+                            <GridContainer justify="center">
+
+                                <GridItem xs={12}>
+                                    <Dropzone
+                                        maxFiles={1}
+                                        getUploadParams={this.getUploadParams}
+                                        onChangeStatus={this.handleChangeStatus}
+                                        onSubmit={this.handleSubmit}
+                                    //accept="image/*,audio/*,video/*,application/pdf/*,word/*"
+                                    />
+                                </GridItem>
+                            </GridContainer>
+
+
+                            {this.state.ShowGrid &&
+                                <GridContainer justify="center" >
+                                    <GridItem xs={12}>
+
+                                        <ReactTable
+                                            title={<h5><TranslationContainer translationKey={"Refund Upload Errors"} /></h5>}
+
+                                            data={this.state.errorList}
+                                            filterable
+                                            columns={this.state.TableDataCopy}
+                                            defaultPageSize={4}
+                                            pageSize={([this.state.errorList.length + 1] < 4) ? [this.state.errorList.length + 1] : 4}
+                                            showPaginationTop={false}
+                                            //showPaginationBottom={([this.state.data.length + 1] <= 5) ? false : true}
+                                            showPaginationBottom={true}
+                                            className="-striped -highlight discription-tab"
+
+                                        />
+
+                                    </GridItem>
+
+                                </GridContainer>}
+                        </CardBody>
+                    </Card>
+                }
+                        </div>
         );
     }
 }
