d9ef22da1526cbc86dda97face6f94dbb6cf4773
Prasant Kumar Singh <prasant.k@inubesolutions.com>
2020-04-19 17:19:35 +0530

    time validation

--- Products/MICA/ClientApp/src/components/CustomDateTimePicker/DateTimePicker.jsx
+++ Products/MICA/ClientApp/src/components/CustomDateTimePicker/DateTimePicker.jsx
@@ -1,13 +1,37 @@
 ﻿
-import React from "react";
+import React, { useState} from "react";
 import { DatePicker, DateTimePicker } from "@material-ui/pickers";
 import MomentUtils from '@date-io/moment';
+import Datetime from "react-datetime";
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+
 function CustomDateTimePicker(props) {
     const { timeformate, labelText, disabled, name, required, id, onChange, value, width, isClearable, minDate, maxDate, disableFuture } = props;
+    const [isAfter, handleDateTime] = React.useState(false);
+    const onChangeFun=(e)=>{
+        console.log("onError", e);
+        if (disableFuture) {
+            var today = e.toDate();
+            var isAfter = Datetime.moment(today).isAfter(new Date());
+            if (isAfter) {
+                handleDateTime(true);
+           
+            } else {
+                handleDateTime(false);
+              
+            }
+            onChange(e);
+          
+        } else {
+            onChange(e);
+        }
+        
 
-
+      
+    }
+  
     return (
+        <div>
         <MuiPickersUtilsProvider utils={MomentUtils} >
             {(timeformate) ?
                 <DateTimePicker
@@ -23,11 +47,12 @@ function CustomDateTimePicker(props) {
                     format="DD/MM/YYYY hh:mm a"
                     autoOk
                     disabled={disabled}
+                    maxDateMessage={<h1>maxDateMessage</h1>}
                     disableFuture={disableFuture}
                     style={{ 'top': '11px', 'width': width }}
                     isClearable={isClearable}
                     //  editFormat={formatter} 
-                    onChange={onChange}
+                    onChange={onChangeFun}
                 />
                 :
 
@@ -54,9 +79,10 @@ function CustomDateTimePicker(props) {
 
             }
 
-
+           
         </MuiPickersUtilsProvider>
-
+            {(isAfter) ? <p style={{ 'top': '15px' }} className="error">Selected Date & Time Cannot be Future Date  & Time</p> : null }
+            </div>
     );
 }
 
