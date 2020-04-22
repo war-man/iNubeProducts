342002a80a6bb1094fdf4c919c7225860ae0eea0
Rashmidevi P <rashmidevi.p@inubesolutions.com>
2020-04-22 12:55:23 +0530

    Document_issue_fixed

--- Products/MICA/ClientApp/src/modules/Claims/views/ClaimIntimate/Document.jsx
+++ Products/MICA/ClientApp/src/modules/Claims/views/ClaimIntimate/Document.jsx
@@ -13,7 +13,7 @@ const MyUploader = (props) => {
 
     console.log("doc props ", props)
     // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+    //const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
 
     // called every time a file's `status` changes
     const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
@@ -82,7 +82,7 @@ const MyUploader = (props) => {
             </CardHeader>
             <GridItem xs={12}>
             <Dropzone 
-                getUploadParams={getUploadParams}
+                //getUploadParams={getUploadParams}
                 onChangeStatus={handleChangeStatus}
                 onSubmit={handleSubmit}
                 accept="image/*,audio/*,video/*,application/pdf/*,word/*"
--- Products/MICA/ClientApp/src/modules/Claims/views/ClaimProcess/DocumentUpload.jsx
+++ Products/MICA/ClientApp/src/modules/Claims/views/ClaimProcess/DocumentUpload.jsx
@@ -15,7 +15,7 @@ const MyUploader = (props) => {
 
     console.log("docclaimId", props.claimId);
     // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
+   // const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
 
     // called every time a file's `status` changes
     const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
@@ -79,7 +79,7 @@ const MyUploader = (props) => {
             </CardHeader>
             <GridItem xs={12}>
                 <Dropzone
-                    getUploadParams={getUploadParams}
+                   // getUploadParams={getUploadParams}
                     onChangeStatus={handleChangeStatus}
                     onSubmit={handleSubmit}
                     accept="image/*,audio/*,video/*,application/pdf/*,word/*"
