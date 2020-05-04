345d045cda929f9b82dbe6bbe666bc092ac3e93c
Chytra M <chytra@inubesolutions.com>
2020-05-04 18:18:11 +0530

    AllocationConfig

--- Products/MICA/ClientApp/src/modules/Allocation/AllocationConfig.js
+++ Products/MICA/ClientApp/src/modules/Allocation/AllocationConfig.js
@@ -1,15 +1,17 @@
 ﻿import config from '../../config';
-let ruleEngineUrl = 'http://localhost:59676';
-if (config.env == "dev") {
-    ruleEngineUrl = 'http://localhost:59676';
+let ruleEngineUrl = config.ruleEngineUrl;
+//if (config.env == "dev") {
+//    ruleEngineUrl = 'http://localhost:59676';
 
-}
-else {
+//}
+//else {
+//    let ruleEngineUrl = config.ruleEngineUrl;
+//  // ruleEngineUrl = 'https://inubecomponentsruleengine.azurewebsites.net';
+//  //  ruleEngineUrl = 'http://localhost:59676';
+////ruleEngineUrl = 'https://egiswitchapi.edelweisscustomeruat.com/api/RuleEngine/swagger/index.html';
+//}
 
-   ruleEngineUrl = 'https://inubecomponentsruleengine.azurewebsites.net';
-  //  ruleEngineUrl = 'http://localhost:59676';
-  
-}
+
 export const AllocationConfig = {
     ruleEngineUrl
 
