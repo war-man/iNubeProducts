79384710e6ef57d6459eb3330ae874be7e865d1f
Ajay V Raikar <ajay.v@inubesolutions.com>
2020-06-03 18:31:33 +0530

    Imports Update

--- Edelweiss/LambdaFunctions/UAT/iNube.Edelweiss.PolicyActive.AwsLambda/iNube.Edelweiss.PolicyActive.AwsLambda/Function.cs
+++ Edelweiss/LambdaFunctions/UAT/iNube.Edelweiss.PolicyActive.AwsLambda/iNube.Edelweiss.PolicyActive.AwsLambda/Function.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
 
 using Amazon.Lambda.Core;
 
