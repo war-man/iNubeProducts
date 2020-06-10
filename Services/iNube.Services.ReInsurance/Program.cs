5342436e74a03e14d6d516829838920c6d9f5e40
Bhavuk Bhardwaj <bhavuk.bhardwaj@inubesolutions.com>
2020-06-10 16:29:24 +0530

    RI AWS Setup - Docker, Builspec File, Task Def , Appsepec  File

--- Services/iNube.Services.ReInsurance/Program.cs
+++ Services/iNube.Services.ReInsurance/Program.cs
@@ -17,9 +17,16 @@ namespace iNube.Services.ReInsurance
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+            return WebHost.CreateDefaultBuilder(args)
                 .UseKestrel(options => options.AddServerHeader = false)
+                .UseUrls($"http://*:{config.GetValue<int>("Host:Port")}")
                 .UseStartup<Startup>();
+        }
     }
 }
