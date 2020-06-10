73f890e574915860eaf545a59797eccb32df05da
sandhya thammineedi <thammineedisandhya@gmail.com>
2020-06-10 17:07:33 +0530

    Scheduler component

---
+++ Components/iNube.Components.Scheduler/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace iNube.Components.Scheduler.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
