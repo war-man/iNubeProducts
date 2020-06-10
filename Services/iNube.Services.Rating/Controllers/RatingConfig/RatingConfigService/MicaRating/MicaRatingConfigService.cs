bfdf55d73d3649094e43f68bc1d1cea7b5f732b8
Dinkar Chandra Choudhary <dinkar.chandra@inubesolutions.com>
2020-06-10 16:40:33 +0530

    RatingExpectionHandle

--- Services/iNube.Services.Rating/Controllers/RatingConfig/RatingConfigService/MicaRating/MicaRatingConfigService.cs
+++ Services/iNube.Services.Rating/Controllers/RatingConfig/RatingConfigService/MicaRating/MicaRatingConfigService.cs
@@ -704,9 +704,22 @@ namespace iNube.Services.Rating.Controllers.RatingConfig.RatingConfigService.Mic
                     
                     expression = rateitem.ExpressionValue;
                     resultExpression = Replace(expression, json_Dictionary);
-                    double result = Convert.ToDouble(new DataTable().Compute(resultExpression, null));
-                    calcultion.Add(new CalculationResult { Entity = rateitem.ExpressionResult, EValue = result.ToString() });
-                    json_Dictionary.Add(rateitem.ExpressionResult, result.ToString());
+                    // For Evaluation {{AllocationMethod}BalanceAmount} Expression and used Catch  
+                    var ExcepExpression = resultExpression;
+                    try
+                    {
+                        double result = Convert.ToDouble(new DataTable().Compute(resultExpression, null));
+                        calcultion.Add(new CalculationResult { Entity = rateitem.ExpressionResult, EValue = result.ToString() });
+                        json_Dictionary.Add(rateitem.ExpressionResult, result.ToString());
+                    }
+                    catch(Exception ex)
+                    {
+                        resultExpression = Replace(ExcepExpression, json_Dictionary);
+                        double result = Convert.ToDouble(new DataTable().Compute(resultExpression, null));
+                        calcultion.Add(new CalculationResult { Entity = rateitem.ExpressionResult, EValue = result.ToString() });
+                        json_Dictionary.Add(rateitem.ExpressionResult, result.ToString());
+                    }
+                    
                     //calcultion.Add(new CalculationResult { Entity = rateitem.ExpressionResult, EValue = (Math.Round((Convert.ToDecimal(result)), 2)).ToString("0.00")
                     //{
                     //    json_Dictionary.Add(tr.Entity, tr.EValue);
