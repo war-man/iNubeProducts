8e669b1749e64104f30763e39c35e5344f138af9
gopikrishnas19 <49558300+gopikrishnas19@users.noreply.github.com>
2020-06-15 21:00:26 +0530

    Deployment_changes

--- Products/MICA/ClientApp/src/config.js
+++ Products/MICA/ClientApp/src/config.js
@@ -21,7 +21,6 @@ let ReinsuranceConfigUrl = Baseurl;
 let ExtensionUrl = Baseurl;
 let NotificationUrl = Baseurl;
 
-
 if (env === "EdelweissProd") {
     Baseurl = 'https://egiswitchapi.edelweissinsurance.com';
     partnerconfigUrl = Baseurl;
