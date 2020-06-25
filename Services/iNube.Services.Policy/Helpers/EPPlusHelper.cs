05883ec2e60174b662a67abf26843ccf6e242771
Nadira Khanum <nadira.khanum@inubesolutions.com>
2020-06-25 12:03:05 +0530

    DataTableExtension

--- Services/iNube.Services.Policy/Helpers/EPPlusHelper.cs
+++ Services/iNube.Services.Policy/Helpers/EPPlusHelper.cs
@@ -94,41 +94,41 @@ namespace iNube.Services.Policy.Helpers
         }
     }
 
-    public static class DataTableExtension
-    {
-        public static List<T> ToList<T>(this DataTable table) where T : class, new()
-        {
-            try
-            {
-                List<T> list = new List<T>();
+    //public static class DataTableExtension
+    //{
+    //    public static List<T> ToList<T>(this DataTable table) where T : class, new()
+    //    {
+    //        try
+    //        {
+    //            List<T> list = new List<T>();
 
-                foreach (var row in table.AsEnumerable())
-                {
-                    T obj = new T();
+    //            foreach (var row in table.AsEnumerable())
+    //            {
+    //                T obj = new T();
 
-                    foreach (var prop in obj.GetType().GetProperties())
-                    {
-                        try
-                        {
-                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
-                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
-                        }
-                        catch
-                        {
-                            continue;
-                        }
-                    }
+    //                foreach (var prop in obj.GetType().GetProperties())
+    //                {
+    //                    try
+    //                    {
+    //                        PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+    //                        propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+    //                    }
+    //                    catch
+    //                    {
+    //                        continue;
+    //                    }
+    //                }
 
-                    list.Add(obj);
-                }
+    //                list.Add(obj);
+    //            }
 
-                return list;
-            }
-            catch
-            {
-                return null;
-            }
-        }
-    }
+    //            return list;
+    //        }
+    //        catch
+    //        {
+    //            return null;
+    //        }
+    //    }
+    //}
 
 }
