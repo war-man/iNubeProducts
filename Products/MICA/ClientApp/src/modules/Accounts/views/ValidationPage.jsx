9843b007c819e9a79a241f354c1f78d298f60f7b
Suyash Pandey <suyash.pandey@inubesolutions.com>
2020-06-26 00:53:13 +0530

    RiIssues

--- Products/MICA/ClientApp/src/modules/Accounts/views/ValidationPage.jsx
+++ Products/MICA/ClientApp/src/modules/Accounts/views/ValidationPage.jsx
@@ -44,6 +44,31 @@
             return true;
         }
         return false;
-    }
+    },
+    verifyNum(value) {
+        //var nameRex = /^((\+){1}91) {1}[1-9]{1}[0-9]{9}$/;
+        var nameRex = /^[1-9]{2}$/;
+        
+        //if (nameRex.test(Number(value))) {
+           
+        //    if ((value) > 0) {
+        //        return false;
+        //    } else {
+        //        return true;
+        //    }
+        //} else {
+        //    return true;
+        //}
+      
+    },
+    verifyEmail(value) {
+        // var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        var emailRex = /^[\w-]+(?:\.[\w-]+)*@(?:[\w-]+\.)+[a-zA-Z]{1,7}$/;
+        if (emailRex.test(value)) {
+            return true;
+        }
+        return false;
+    },
 }
 export default validationPage;
