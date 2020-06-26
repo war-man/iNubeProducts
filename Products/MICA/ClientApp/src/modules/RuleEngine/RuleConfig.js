81abf7c54d2573d8287c70542deea8d3182f4b27
Dinkar Chandra Choudhary <dinkar.chandra@inubesolutions.com>
2020-06-26 12:25:16 +0530

    RuleEngine

--- Products/MICA/ClientApp/src/modules/RuleEngine/RuleConfig.js
+++ Products/MICA/ClientApp/src/modules/RuleEngine/RuleConfig.js
@@ -7,7 +7,7 @@ if (config.env == "dev") {
 else {
 
     //ruleEngineUrl = 'https://inubecomponentsruleengine.azurewebsites.net';
-    ruleEngineUrl = 'http://dev2-publi-3o0d27omfsvr-1156685715.ap-south-1.elb.amazonaws.com/api/RuleEngine/swagger/index.html';
+    ruleEngineUrl = 'http://dev2-publi-3o0d27omfsvr-1156685715.ap-south-1.elb.amazonaws.com';
 
 }
 export const ruleconfig = {
