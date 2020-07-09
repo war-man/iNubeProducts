76b5c374b0ca7d7bc569904b8d39453e347c5c0c
Chytra M <chytra@inubesolutions.com>
2020-07-09 14:20:41 +0530

    config

--- Products/MICA/ClientApp/src/config.js
+++ Products/MICA/ClientApp/src/config.js
@@ -2,7 +2,8 @@
 //let env = "Production";
 let env = "Dev";               
 //let env = "Local";                      
-//let env = "UAT";      
+//let env = "UAT"; 
+
  
 let Baseurl = 'http://dev2-publi-3o0d27omfsvr-1156685715.ap-south-1.elb.amazonaws.com';
 let partnerconfigUrl = Baseurl;   
