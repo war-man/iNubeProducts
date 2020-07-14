e7bc0d17361f1bc8e89f887255292f4cb85e2ba7
Ajay V Raikar <ajay.v@inubesolutions.com>
2020-07-14 17:32:38 +0530

    AWSTest Startup issue fixed

--- Services/AWSServiceTesting/Startup.cs
+++ Services/AWSServiceTesting/Startup.cs
@@ -23,6 +23,10 @@ namespace AWSServiceTesting
         {
             Configuration = configuration;
         }
+        public class AppSettings
+        {
+            public string Secret { get; set; }
+        }
 
         public IConfiguration Configuration { get; }
 
@@ -30,58 +34,13 @@ namespace AWSServiceTesting
         public void ConfigureServices(IServiceCollection services)
         {
 
-            //services.AddDbContext<MICARIContext>();
-          //  var micaconnectionstring = Configuration.GetConnectionString("RIConnection");
-            //services.AddDbContext<MICAACContext>(x => x.UseSqlServer(micaconnectionstring));
-
-
             var appSettingsSection = Configuration.GetSection("AppSettings");
-           // services.Configure<AppSettings>(appSettingsSection);
-
-            ConfigureModuleService(services);
-         //   services.InitializedCommonServices(Configuration);
-
-            services.AddMvc(); 
-           //.AddFluentValidation(options =>
-           //{
-           //    options.RegisterValidatorsFromAssemblyContaining<Startup>();
-           //});
-           // services.AddHealthChecks().AddSqlServer(micaconnectionstring);
-           // services.AddAutoMapper(typeof(Startup));
-
-
-
-
-            // var connectionstring = Configuration.GetConnectionString("RIConnection");
-
-            // var appSettingsSection = Configuration.GetSection("AppSettings");
-            // services.Configure<AppSettings>(appSettingsSection);
-
-            // //Module service
-            // ConfigureModuleService(services);
-
-            // //Common Service
-            // services.InitializedCommonServices(Configuration);
-
-            // services.AddMvc()
-            //.AddFluentValidation(options =>
-            //{
-            //    options.RegisterValidatorsFromAssemblyContaining<Startup>();
-            //});
-
-
-            // var appSettings = Configuration.GetSection("AppSettings");
-            // services.Configure<AppSettings>(appSettings);
-
-
-            // services.AddHealthChecks().AddSqlServer(connectionstring);
-            // services.AddAutoMapper(typeof(Startup));
+            services.Configure<AppSettings>(appSettingsSection);         
+            services.AddMvc();           
         }
 
         public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
-        {
-            app.InitializedCommonConfiguration(env, Configuration);
-            // app.ConfigureExceptionHandler(new LoggerManager(Configuration));
+        {            
             app.ConfigureCustomExceptionMiddleware(new LoggerManager(Configuration));
             if (env.IsDevelopment())
             {
@@ -89,7 +48,6 @@ namespace AWSServiceTesting
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseAuthentication();
@@ -97,33 +55,5 @@ namespace AWSServiceTesting
             app.UseMvc();
         }
 
-        private void ConfigureModuleService(IServiceCollection services)
-        {
-            // configure DI for application services
-            // services.AddTransient<IValidator<RIModels>, CreateProductRequestValidator>();
-            //services.AddTransient<IEmailService, EmailService>();
-            //services.AddTransient<MicaAccountConfigService>();
-            //services.AddTransient<MotorAccountConfigService>();
-            //services.AddTransient<AvoAccountConfigService>();
-            //services.AddTransient<Func<string, IAccountingConfigService>>(serviceProvider => lkey =>
-            //{
-            //    switch (lkey)
-            //    {
-            //        case "Mica":
-            //            return serviceProvider.GetService<MicaAccountConfigService>();
-            //        case "Motor":
-            //            return serviceProvider.GetService<MotorAccountConfigService>();
-            //        case "Avo":
-            //            return serviceProvider.GetService<AvoAccountConfigService>();
-            //        default:
-            //            return serviceProvider.GetService<MicaAccountConfigService>();
-
-            //    }
-            //});
-            //services.AddScoped<IReInsuranceService, ReInsuranceService>();
-            //services.AddScoped<IIntegrationService, IntegrationService>();
-
-
-        }
     }
 }
